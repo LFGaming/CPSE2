41b924530c713a767fd53dec3dee5492da25fa33
LFGaming <github@luke.irtl.nl>
2023-11-30 15:22:33 +0100

    moving and remembering works now

--- factory/main.cpp
+++ factory/main.cpp
@@ -13,10 +13,12 @@ struct Shape {
     std::string type;
     sf::Vector2f position;
     sf::Vector2f end; // Second point for lines and rectangles
-    float size; // Diameter for circles
+    // float size; // Diameter for circles
     sf::Color color;
     std::string textureFile; // Filename of the texture for pictures
     sf::Vector2f scale; // Scale factor for pictures
+    bool isDragging = false;
+    sf::Vector2f size;
 };
 
 std::vector<Shape> readShapesFromFile(const std::string& filename) {
@@ -33,22 +35,21 @@ std::vector<Shape> readShapesFromFile(const std::string& filename) {
         std::istringstream iss(line);
         Shape shape;
         int r, g, b;
-        if (iss >> shape.type) {
-            if (shape.type == "circle") {
-                iss >> shape.position.x >> shape.position.y >> shape.size >> r >> g >> b;
+        if (iss >> shape.type >> shape.position.x >> shape.position.y) {
+            if (shape.type == "circle" || shape.type == "rectangle") {
+                iss >> shape.size.x >> shape.size.y >> r >> g >> b;
                 shape.color = sf::Color(r, g, b);
-            } else if (shape.type == "rectangle" || shape.type == "line") {
-                iss >> shape.position.x >> shape.position.y >> shape.end.x >> shape.end.y >> r >> g >> b;
+            } else if (shape.type == "line") {
+                iss >> shape.end.x >> shape.end.y >> r >> g >> b;
                 shape.color = sf::Color(r, g, b);
-            }if (shape.type == "picture") {
-                iss >> shape.position.x >> shape.position.y >> shape.textureFile >> shape.scale.x >> shape.scale.y;
+            } else if (shape.type == "picture") {
+                iss >> shape.textureFile >> shape.scale.x >> shape.scale.y;
             }
             shapes.push_back(shape);
         } else {
             std::cerr << "Error reading line: " << line << std::endl;
         }
     }
-
     return shapes;
 }
 
@@ -133,24 +134,27 @@ while (window.isOpen()) {
 
     window.clear();
 
-for (const auto& shape : shapes) {
-    if (shape.type == "circle") {
-        sf::CircleShape circle(shape.size / 2); // Radius is half the diameter
-        circle.setPosition(shape.position);
-        circle.setFillColor(shape.color);
-        window.draw(circle);
-    } else if (shape.type == "rectangle") {
-        sf::RectangleShape rectangle(shape.end - shape.position);
-        rectangle.setPosition(shape.position);
-        rectangle.setFillColor(shape.color);
-        window.draw(rectangle);
-    } else if (shape.type == "line") {
-        sf::Vertex line[] = {
-            sf::Vertex(shape.position, shape.color),
-            sf::Vertex(shape.end, shape.color)
-        };
-        window.draw(line, 2, sf::Lines);
-        }else if (shape.type == "picture") {
+    for (const auto& shape : shapes) {
+        sf::FloatRect bounds;
+        if (shape.type == "circle") {
+            sf::CircleShape circle(shape.size.x / 2); // Radius is half the diameter
+            circle.setPosition(shape.position);
+            circle.setFillColor(shape.color);
+            window.draw(circle);
+            bounds = circle.getGlobalBounds();
+        } else if (shape.type == "rectangle") {
+            sf::RectangleShape rectangle(shape.size);
+            rectangle.setPosition(shape.position);
+            rectangle.setFillColor(shape.color);
+            window.draw(rectangle);
+            bounds = rectangle.getGlobalBounds();
+        } else if (shape.type == "line") {
+            sf::Vertex line[] = {
+                sf::Vertex(shape.position, shape.color),
+                sf::Vertex(shape.end, shape.color)
+            };
+            window.draw(line, 2, sf::Lines);
+        } else if (shape.type == "picture") {
             sf::Texture texture;
             if (!texture.loadFromFile(shape.textureFile)) {
                 std::cerr << "Error loading texture: " << shape.textureFile << std::endl;
@@ -158,12 +162,10 @@ for (const auto& shape : shapes) {
             }
             sf::Sprite sprite(texture);
             sprite.setPosition(shape.position);
-
-            // Calculate the scale factor based on the original size and the desired size
             sf::Vector2u originalSize = texture.getSize();
             sprite.setScale(shape.scale.x / originalSize.x, shape.scale.y / originalSize.y);
-
             window.draw(sprite);
+            bounds = sprite.getGlobalBounds();
         }
     }
 
@@ -178,13 +180,73 @@ for (const auto& shape : shapes) {
 
     sf::sleep(sf::milliseconds(20));
 
-    sf::Event event;        
+sf::Event event;        
     while(window.pollEvent(event)) {
         if(event.type == sf::Event::Closed) {
             window.close();
+        } else if (event.type == sf::Event::MouseButtonPressed) {
+            if (event.mouseButton.button == sf::Mouse::Left) {
+                sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                for (auto& shape : shapes) {
+                    sf::FloatRect bounds;
+                    if (shape.type == "circle") {
+                        sf::CircleShape circle(shape.size.x / 2);
+                        circle.setPosition(shape.position);
+                        bounds = circle.getGlobalBounds();
+                    } else if (shape.type == "rectangle") {
+                        sf::RectangleShape rectangle(shape.size);
+                        rectangle.setPosition(shape.position);
+                        bounds = rectangle.getGlobalBounds();
+                    } else if (shape.type == "picture") {
+                        sf::Texture texture;
+                        if (!texture.loadFromFile(shape.textureFile)) {
+                            std::cerr << "Error loading texture: " << shape.textureFile << std::endl;
+                            continue;
+                        }
+                        sf::Sprite sprite(texture);
+                        sprite.setPosition(shape.position);
+                        sf::Vector2u originalSize = texture.getSize();
+                        sprite.setScale(shape.scale.x / originalSize.x, shape.scale.y / originalSize.y);
+                        bounds = sprite.getGlobalBounds();
+                    }
+                    if (bounds.contains(mousePos)) {
+                        shape.isDragging = true;
+                    }
+                }
+            }
+        } else if (event.type == sf::Event::MouseButtonReleased) {
+            if (event.mouseButton.button == sf::Mouse::Left) {
+                for (auto& shape : shapes) {
+                    shape.isDragging = false;
+                }
+            }
         }
     }
-} // This is the missing closing brace
+
+    sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+    for (auto& shape : shapes) {
+        if (shape.isDragging) {
+            sf::Vector2f oldPosition = shape.position;
+            shape.position = mousePos;
+            if (shape.type == "rectangle") {
+                shape.end = shape.position + shape.size;
+            }
+        }
+    }
+}
+std::ofstream file("objects.txt");
+for (const auto& shape : shapes) {
+    if (shape.type == "circle" || shape.type == "rectangle") {
+        file << shape.type << " " << shape.position.x << " " << shape.position.y << " " << shape.size.x << " " << shape.size.y << " "
+             << (int)shape.color.r << " " << (int)shape.color.g << " " << (int)shape.color.b << "\n";
+    } else if (shape.type == "line") {
+        file << shape.type << " " << shape.position.x << " " << shape.position.y << " " << shape.end.x << " " << shape.end.y << " "
+             << (int)shape.color.r << " " << (int)shape.color.g << " " << (int)shape.color.b << "\n";
+    } else if (shape.type == "picture") {
+        file << shape.type << " " << shape.position.x << " " << shape.position.y << " " << shape.textureFile << " " << shape.scale.x << " " << shape.scale.y << "\n";
+    }
+}
+file.close();
 
 std::cout << "Terminating application\n";
 return 0;
